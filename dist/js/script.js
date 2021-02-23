d38ffdb4226fc9c26b8438a5f30aec6b31bfdd44
Denis <denis.mazuryk@gmail.com>
2021-02-23 16:09:01 +0100

    before portfolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,13 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });  
+
+
+const counters = document.querySelectorAll('.liner__procent'),
+      liners = document.querySelectorAll('.progress__iteam-load'); 
+
+
+
+      counters.forEach( (iteam, i) => {
+        liners[i].style.width = iteam.innerHTML;
+      });
